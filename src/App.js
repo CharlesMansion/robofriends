297abae524dd00a25070d12069cbe5a8b4db3dd3
Nicolas Nollomont <nicolasnollomont@iMac-de-Nicolas.lan>
2018-04-25 13:32:03 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import { robots } from './Robots';
 import Searchbox from './Searchbox';
 
-const App = () => {
-	return (
-		<div className="tc">
-			<h1>Robofriends</h1>
-			<Searchbox />
-			<Cardlist robots={robots}/>
-		</div>
-	)
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+		robots: robots,
+		searchfield: ''
+		}
+	}
+	render() {
+		return (
+			<div className="tc">
+				<h1>Robofriends</h1>
+				<Searchbox />
+				<Cardlist robots={robots}/>
+			</div>
+		)
+	}
 }
-
 export default App;
