d146a6676c077247a5f5e960561dc024558c3805
Nicolas Nollomont <nicolasnollomont@iMac-de-Nicolas.lan>
2018-05-03 19:32:18 +0200

    adding files

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Scroll = (props) => {
-	return <div style={{overflowY: "scroll", height: "800px"}}>{props.children}</div>
+	return <div style={{overflowY: "scroll", height: "1000px"}}>{props.children}</div>
 };
 
 export default Scroll;
